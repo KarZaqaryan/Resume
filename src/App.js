dbafe0de6a884b1250aa21e390adc49007327241
KarZaqaryan <karenzaqaryan627@gmail.com>
2025-01-14 10:09:18 +0400

    Full Project

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import Main from "./page/main";
 
 function App() {
-    return (
-        <div className="App">
-            <Main/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  );
 }
 
 export default App;
--- src/page/main.jsx
+++ src/page/main.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import About from "../component/about";
+import Experiance from "../component/experiance";
+import Education from "../component/education";
+import Rating from "../component/rating";
+import Languages from "../component/languages";
+import Hobbies from "../component/hobbies";
+import Certeficate from "../component/certeficate";
 
 function Main(props) {
     return (
-        <div className={"container m-auto main_div"}>
+        <div className={"container m-auto"}>
             <div className="row">
-              <About/>
+                <About/>
 
                 <div className="col-md-8 col-lg-9 ">
                     <div className="content">
-
+                        <Experiance/>
+                        <Education/>
+                        <Rating/>
+                        <Languages/>
+                        <Hobbies/>
+                        <Certeficate/>
                     </div>
                 </div>
 
